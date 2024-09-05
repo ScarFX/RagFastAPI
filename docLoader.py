5253ca53b7850cff0c18909f076e51066a8c2000
Connor <36115510+ScarFX@users.noreply.github.com>
2024-09-05 10:01:52 -0400

    Added different file types/links

---
+++ docLoader.py
@@ -0,0 +1,72 @@
+from sys import exception
+from langchain_community.document_loaders import (
+    WebBaseLoader,
+    TextLoader,
+    PyPDFLoader,
+    CSVLoader,
+    Docx2txtLoader,
+    UnstructuredEPubLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredXMLLoader,
+    UnstructuredRSTLoader,
+    UnstructuredExcelLoader,
+    UnstructuredPowerPointLoader,
+    YoutubeLoader
+)
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+async def load_file(filename:str,filepath:Path,metadata:dict):    
+    file_ext = filename.split(".")[-1].lower()
+    docs = [] 
+    splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1024,
+            chunk_overlap=0,
+            length_function=len,
+            is_separator_regex=False
+            )
+    match file_ext: 
+        case "txt":
+            loader = TextLoader(filepath, autodetect_encoding=True)
+            wholeDoc = loader.aload()
+            docs = splitter.split_documents(wholeDoc)
+
+        case "csv":
+            loader = CSVLoader(filepath)
+            docs = loader.load()
+        case "pdf":
+            loader = PyPDFLoader(file_path=filepath)
+            wholeDoc = loader.load()
+            docs = splitter.split_documents(wholeDoc)
+        case "docx":
+            loader = Docx2txtLoader(filepath)
+            wholeDoc = loader.load()
+            docs = splitter.split_documents(wholeDoc)
+        case _:
+            raise TypeError(f'The file extension {file_ext} is not supported')
+    for doc in docs:
+        doc.metadata = doc.metadata | metadata 
+    return docs
+
+async def load_link(url:str, metadata:dict = None):
+    website = url.split(".")[1].lower()
+    docs = []
+    splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1024,
+            chunk_overlap=0,
+            length_function=len,
+            is_separator_regex=False
+            )
+    match website:
+        case 'youtube':
+            loader = YoutubeLoader.from_youtube_url(url, add_video_info=False)
+            wholeDoc = loader.load()
+            docs = splitter.split_documents(wholeDoc)
+        case _:  #assume html webpage
+            loader = WebBaseLoader(url)
+            wholeDoc = loader.load()
+            docs = splitter.split_documents(wholeDoc)
+    if metadata is not None:
+        for doc in docs:
+            doc.metadata = doc.metadata | metadata 
+    return docs
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing import process
 from fastapi import FastAPI, UploadFile,Query,Path, Body, File, Depends, HTTPException, Form
 from dotenv import load_dotenv
 import uvicorn
@@ -19,8 +20,11 @@ import asyncio
 from datetime import datetime
 from langchain_chroma import Chroma
 import chromadb
-from typing import Any,Dict, List
+from typing import Any,Dict, List, Optional
 from storage import VectorStorage
+from docLoader import *
+import shutil
+from pathlib import Path
 
 
 load_dotenv() 
@@ -34,9 +38,6 @@ chat = ChatOpenAI(
     openai_api_key=os.environ["OPENAI_API_KEY"],
     model='gpt-4o-mini-2024-07-18'
 )
-# messages = [
-#     SystemMessage(content="You are a helpful assistant."),
-# ] not state transfer 
 
 
 @app.get("/")
@@ -62,24 +63,36 @@ async def upload_file(files: List[UploadFile] = (File(...)), metaJson: str= Form
     except json.JSONDecodeError:
         return {"error":"Invalid JSON"}
     tasks = []
+    docData = []
+    allowed_types = {
+        'text/csv',
+        'text/plain',
+        'application/pdf',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        }
     for file in files:
-        if file.content_type != 'text/plain':
+        if file.content_type not in allowed_types : #check type
             raise HTTPException(status_code=400, detail=f"{file.content_type} is an Invalid file type")
-        content = await file.read() #Does read big files? 
-        txt =  content.decode('utf-8')
-        task = asyncio.create_task(embedStore(txt, metadata))
+        file_location = Path("uploads") / file.filename #write location
+        with file_location.open('wb') as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        #load_file() is async 
+        docData.extend(await load_file(file.filename,file_location,metadata)) 
+        task = asyncio.create_task(process_documents(docData))
         tasks.append(task)
     await asyncio.gather(*tasks)  
-    return {"filename": file.filename, "message": "File/s uploaded successfully"}
+    return {"filename": file.filename, "message": "File/s written successfully"}
 
-async def embedStore(txt: str, metaData ) -> None: 
-    data = split_into_chunks(txt, 2048) # roughly 20ish lines of text 
-    docData = [Document(
-        page_content=doc,
-        metadata=metaData
-        ) for doc in data]
+#Upload a url
+@app.post("/links")
+async def upload_url(url:str = Form(...,description="link to pull data from"), metaJson: str = Form(...)):
+    try:
+        metadata: Dict[str, Any] = json.loads(metaJson)
+    except json.JSONDecodeError:
+        return {"error":"Invalid JSON"}
+    docData = await load_link(url)
     await process_documents(docData)
-
+    return {"URL: ": url, "message": "Link successfully uplaoded "}
 async def process_documents(docData: list[Document],
                              batch_size: int = 20, concurrent_max: int = 10):
     semaphore = asyncio.Semaphore(concurrent_max)
@@ -95,28 +108,6 @@ async def process_batch(batch: list[Document], semaphore):
     async with semaphore: 
         await vector_storage.aadd_documents(batch) #Call to wrapper method
 
-def split_into_chunks(text: str, max_tokens: int) -> list[str]:
-    words = text.split()
-    chunks = []
-    current_chunk = []
-    current_length = 0
-
-    for word in words:
-        word_length = len(word) + 1  # +1 for space
-        if current_length + word_length > max_tokens:
-            # Finalize current chunk
-            chunks.append(' '.join(current_chunk))
-            current_chunk = [word]
-            current_length = word_length
-        else:
-            current_chunk.append(word)
-            current_length += word_length
-
-    # Add any remaining words as a final chunk
-    if current_chunk:
-        chunks.append(' '.join(current_chunk))
-
-    return chunks
 
 def similarDocs(query: str) -> str:
     results =  vector_storage.similarity_search(query, k=2) #wrapper method
